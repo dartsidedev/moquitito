f162be8d134d5ac60805d86e2aba0e3c801e0302
Vince Varga <vince.varga@serialdeveloper.com>
2020-08-22 20:52:50 +0200

    Initialize package: stagehand console-full

---
+++ bin/main.dart
@@ -0,0 +1,6 @@
+import 'package:moquitito/moquitito.dart' as moquitito;
+
+void main(List<String> arguments) {
+  print('args: $arguments');
+  print('Hello world: ${moquitito.calculate()}!');
+}
---
+++ test/moquitito_test.dart
@@ -0,0 +1,8 @@
+import 'package:moquitito/moquitito.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
