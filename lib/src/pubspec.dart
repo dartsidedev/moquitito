63f0bc3f497731dd2d21289ab12a6c0aef7eedf8
Vince Varga <vince.varga@serialdeveloper.com>
2020-08-23 18:06:12 +0200

    Add description and version to pubspec.yaml

---
+++ lib/src/pubspec.dart
@@ -0,0 +1,7 @@
+// Generated file. Do not modify.
+//
+// This file is generated using the build_pubspec package.
+// For more information, go to: https://pub.dev/packages/build_pubspec
+const String description = '''MQTT command-line client written in Dart. Inspired by mosquitto_pub/sub.''';
+const String name = '''moquitito''';
+const String version = '''0.1.0-dev''';
